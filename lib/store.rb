b6e1237fd7562a169ffabac01eb463d33fe8b7e9
cspraggett <cspraggett@gmail.com>
2020-03-02 22:06:40 +0000

    do validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  #           :annual_revenue.is_a? Integer 
+  #           :annual_revenue >= 0
+            # :must_carry_mens
+
+#   def must_carry_mens
+#     if mens_apparel == false #&& womens_apparel: false
+#       errors.add(mens_apparel: 'is not present')
+#     end
 end
